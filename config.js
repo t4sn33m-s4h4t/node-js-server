2d8086e881f60a3aaed9de8f0e10906b71f301b4
t4sn33m-s4h4t <mdsahat6397@gmail.com>
2023-04-02 22:36:28 +0600

    done

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    serverUrl: 'https://tasneem-node-server.onrender.com/'
+  };
+  
+  module.exports = config;
--- index.js
+++ index.js
@@ -50,6 +50,71 @@ app.delete("/api/images/:name", (req, res) => {
   });
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+// portfolio starts
+
+
+app.use("/portfolio", express.static(path.join(__dirname, "/portfolio")));
+
+// Serve uploaded image files
+const portfolioImageStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "portfolio");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.category + "_" + Date.now() + "_" + file.originalname);
+  },
+});
+
+const uploadPortfolioImage = multer({ storage: portfolioImageStorage });
+
+app.post("/api/uploadPortfolio", uploadPortfolioImage.array("files"), (req, res) => {
+  res.status(200).json("Files have been uploaded");
+});
+app.get('/api/portfolioImages', (req, res) => {
+  fs.readdir('portfolio', (err, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      const imageUrls = files.map(file => `/portfolio/${file}`);
+      res.status(200).json(imageUrls);
+    }
+  });
+});
+
+
+
+app.delete("/api/portfolio/:name", (req, res) => {
+  const fileName = req.params.name;
+  const filePath = path.join(__dirname, "portfolio", fileName);
+
+
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json("Error deleting image file");
+    } else {
+      res.status(200).json("Image file deleted successfully");
+    }
+  });
+});
+
+
+
+// portfolio ends
+
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL, {
